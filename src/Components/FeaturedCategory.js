642def66c3c6b79fc90b5afc68c97ce7f96f9f08
sumit hariyani <sumit.hariyani1@gmail.com>
2022-04-27 20:59:53 +0530

    test commit10

--- src/Components/FeaturedCategory.js
+++ src/Components/FeaturedCategory.js
@@ -29,7 +29,7 @@ const get_cat = async () =>{
                 "type": "formData"}};
                    console.log(formData); 
                 try{
-                  let response = await axios.post('https://cws.in.net/iron_gate/admin/api/get_category',formData,options);
+                  let response = await axios.post('https://hippotheftprotection.in/burp/api/user',formData,options);
                   return   response.data;
                 } catch(err){ console.error(err); toast.error('some errror'); return false;  }
              }  
